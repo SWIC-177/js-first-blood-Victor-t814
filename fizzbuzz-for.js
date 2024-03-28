a45648c4fe05c8de2717061ae5c0bfdb2d562b32
Victor-t814 <victor.trail001@student.swic.edu>
2024-03-28 11:24:47 -0500

    update fizzbuzz-for

--- fizzbuzz-for.js
+++ fizzbuzz-for.js
@@ -1,3 +1,11 @@
-for (let num = 1; num <= 100; num += 1) {
-  console.log(num);
+for (let i = 1; i <= 100; i += 1) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
 }

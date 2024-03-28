fe75d0b10df16e202544a15ba585278c25e3e298
Victor-t814 <victor.trail001@student.swic.edu>
2024-03-28 11:57:24 -0500

    update index.js array

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const person = {
-  name: "John",
-  age: 20,
-};
+const people = [
+  {
+    name: "John",
+    age: 17,
+  },
+  {
+    name: "Jane",
+    age: 20,
+  },
+  {
+    name: "Mary",
+    age: 25,
+  },
+  {
+    name: "Peter",
+    age: 30,
+  },
+  {
+    name: "Paul",
+    age: 35,
+  },
+];
 
 function verifyAdulthood(personName, personAge) {
   if (personAge >= 18) return `Welcome, ${personName}!`;
 
   return `You are not old enough to enter, ${personName}.`;
 }
-
-const adultMessage = verifyAdulthood(person.name, person.age);
-
-console.log(adultMessage);
+people.forEach((person) => {
+  console.log(verifyAdulthood(person.name, person.age));
+});
